46777ae7ad55902645e7e462c6a849b8df6d8b29
Sirska-Mariia <149333906+Sirska-Mariia@users.noreply.github.com>
2024-11-04 23:36:39 +0200

    Update DecoratorTest.java

--- src/test/java/ucu/edu/ua/DecoratorTest.java
+++ src/test/java/ucu/edu/ua/DecoratorTest.java
@@ -14,8 +14,8 @@ public class DecoratorTest {
         Item flower = new Flower(10, 5, FlowerType.ROSE);
         Item ribbonFlower = new RibbonDecorator(flower);
 
-      //  assertEquals("Flower type: ROSE, color: #0000FF, sepal length: 5.0 with ribbon", ribbonFlower.getDescription());
-     //   assertEquals(15.0, ribbonFlower.getPrice());
+        assertEquals("Flower type: ROSE, color: #0000FF, sepal length: 5.0 with ribbon", ribbonFlower.getDescription());
+        assertEquals(15.0, ribbonFlower.getPrice());
     }
 
     @Test
@@ -23,8 +23,8 @@ public class DecoratorTest {
         Item flower = new Flower(10, 7, FlowerType.TULIP);
         Item decoratedFlower = new BasketDecorator(new PaperDecorator(new RibbonDecorator(flower)));
 
-     //   assertEquals("Flower type: TULIP, color: #0000FF, sepal length: 7.0 with ribbon wrapped in paper placed in a basket", 
-      //  decoratedFlower.getDescription());
-      //  assertEquals(28.0, decoratedFlower.getPrice());
+        assertEquals("Flower type: TULIP, color: #0000FF, sepal length: 7.0 with ribbon wrapped in paper placed in a basket", 
+        decoratedFlower.getDescription());
+        assertEquals(28.0, decoratedFlower.getPrice());
     }
 }
